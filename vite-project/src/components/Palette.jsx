6074f9efc0aee602cb9dd68c19dfa79f298d7425
LaneBrents <lanebrents@gmail.com>
2023-10-14 14:31:17 -0400

    Added Seeded colors and palette component

---
+++ vite-project/src/components/Palette.jsx
@@ -0,0 +1,11 @@
+export default function Palette() {
+    return (
+        <div className="palette">
+            {/* navbar goes here */}
+            <div className="palette-colors">
+            {/* color boxed goes here */}
+            </div>
+            {/* footer goes here */}
+        </div>
+    )
+}
