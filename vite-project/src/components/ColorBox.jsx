86e74b3fc1315a592aec8c4973b8e89ed9f24997
LaneBrents <lanebrents@gmail.com>
2023-10-15 16:55:17 -0400

    ColorBox Styling

--- vite-project/src/components/ColorBox.jsx
+++ vite-project/src/components/ColorBox.jsx
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
 import "../styles/ColorBox.css";
 
-export default function ColorBox(props) {
+export default function ColorBox({name, background}) {
   return (
-    <div style={{ background: props.background }} className="ColorBox">
-      <span>{props.name}</span>
-      <span> More</span>
+    <div style={{ background: background }} className="ColorBox">
+      <div className="container">
+        <div className="container-content">
+            <span>{name}</span>
+        </div>
+        <button className="copy-btn">Copy</button>
+      </div>
+      <button className="more-btn">More</button>
     </div>
   );
 }
--- vite-project/src/components/Palette.jsx
+++ vite-project/src/components/Palette.jsx
@@ -2,8 +2,8 @@ import React, { Component } from "react";
 import ColorBox from "./ColorBox";
 import "../styles/Palette.css";
 
-export default function Palette(props) {
-  const colorBoxes = props.colors.map((color) => (
+export default function Palette({colors}) {
+  const colorBoxes = colors.map((color) => (
     <ColorBox background={color.color} name={color.name} />
   ));
 
