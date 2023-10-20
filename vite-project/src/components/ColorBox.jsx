35fa3dcf8b3dce10a3f00675301c8ce3fffaca1f
LaneBrents <lanebrents@gmail.com>
2023-10-20 16:06:59 -0400

    Added click-to-copy animation and component

--- vite-project/src/components/ColorBox.jsx
+++ vite-project/src/components/ColorBox.jsx
@@ -1,19 +1,36 @@
-import React, { Component } from "react";
-import {CopyToClipboard} from "react-copy-to-clipboard"
+import React, { Component, useState, useEffect } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
 import "../styles/ColorBox.css";
 
-export default function ColorBox({name, background}) {
+export default function ColorBox({ name, background }) {
+  const [copied, setCopied] = useState(false);
+
+  const changeCopyState = () => {
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 1500);
+  };
+
   return (
-    <CopyToClipboard text={background}>
-    <div style={{ background: background }} className="ColorBox">
-      <div className="container">
-        <div className="container-content">
-            <span>{name}</span>
+    <CopyToClipboard text={background} onCopy={changeCopyState}>
+      <div style={{ background }} className="ColorBox">
+        <div
+          style={{ background }}
+          className={`copy-overlay ${copied && "show"}`}
+        />
+        <div className={`copy-msg ${copied && "show"}`}>
+          <h1>copied!</h1>
+          <p>{background}</p>
         </div>
-        <button className="copy-btn">Copy</button>
+        <div className="container">
+          <div className="container-content">
+            <span>{name}</span>
+          </div>
+          <button className="copy-btn">Copy</button>
+        </div>
+        <button className="more-btn">More</button>
       </div>
-      <button className="more-btn">More</button>
-    </div>
     </CopyToClipboard>
   );
 }
