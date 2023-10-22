daff551e3d4b7992a904802d74a42082c25e44b8
LaneBrents <lanebrents@gmail.com>
2023-10-22 16:35:20 -0400

    Added MaterialUI and working color slider

--- vite-project/src/components/Palette.jsx
+++ vite-project/src/components/Palette.jsx
@@ -1,14 +1,33 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import ColorBox from "./ColorBox";
 import "../styles/Palette.css";
+import { Slider } from "@mui/material";
 
-export default function Palette({colors}) {
-  const colorBoxes = colors.map((color) => (
-    <ColorBox background={color.color} name={color.name} />
+export default function Palette({ colors, palette }) {
+  const [level, setLevel] = useState(500);
+
+  const changeLevel = (event, level) => {
+    setLevel(level);
+  };
+
+  const colorBoxes = palette.colors[level].map((color) => (
+    <ColorBox background={color.hex} name={color.name} />
   ));
 
   return (
     <div className="Palette">
+      <Slider
+        level={level}
+        onChange={changeLevel}
+        className="slider"
+        aria-label="Temperature"
+        value={level}
+        valueLabelDisplay="auto"
+        step={100}
+        marks
+        min={100}
+        max={900}
+      />
       {/* navbar goes here */}
       <div className="Palette-Colors">{colorBoxes}</div>
       {/* footer goes here */}
