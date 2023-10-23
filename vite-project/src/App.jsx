9d02168a6d2c02a403c8587f7253f8bfd1c234f5
LaneBrents <lanebrents@gmail.com>
2023-10-23 18:57:41 -0400

    Add React-Router-Dom

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,5 +1,5 @@
 import react, { useState } from 'react'
-// import './App.css'
+import { Route } from 'react-router-dom';
 
 import generatePalette from './components/colorHelpers';
 import seedColors from './components/seedColors';
@@ -7,6 +7,10 @@ import Palette from './components/Palette';
 
 function App() {
   return (
+    // <>
+    //   <Route exact path="/#" render={() => <h1>Palette goes here</h1>}/>
+    //   <Route exact path="/palette/:id" />
+    // </>
       <div>
         <Palette palette={generatePalette(seedColors[4])} />
       </div>
